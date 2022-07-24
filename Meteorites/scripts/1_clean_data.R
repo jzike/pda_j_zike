b6d4f4ecb2c724e69a48e1f529123347f70664fe
jzike <107878458+jzike@users.noreply.github.com>
2022-07-24 17:39:49 +0100

    Adding project files

---
+++ Meteorites/scripts/1_clean_data.R
@@ -0,0 +1,50 @@
+# Libraries ----
+
+library(tidyverse)
+library(assertr)
+
+# Data ----
+
+meteorites <- read_csv("data/meteorite_landings.csv")
+
+# Data cleaning ----
+
+## Tidy and format data ----
+
+# Tidy column names
+meteorites_cleaned <- meteorites %>% 
+janitor::clean_names()
+
+# Split latitude and longitude into separate variables and convert to numeric
+meteorites_cleaned <- meteorites_cleaned %>% 
+  #Split latitude and longitude into separate columns
+  separate(geo_location, c("latitude", "longitude"), sep = ",") %>% 
+  #Remove unnecessary parentheses and spaces from lat and long values
+  mutate(latitude = str_replace_all(latitude, "[(]", "")) %>% 
+  mutate(longitude = str_replace_all(longitude, "[ )]", "")) %>% 
+  #Convert latitude and longitude into numeric values
+  mutate(latitude = as.numeric(unlist(latitude))) %>% 
+  mutate(longitude = as.numeric(unlist(longitude)))
+
+## Missing values ----
+
+# Replaces NAs in lat and long variables with 0
+meteorites_cleaned <- meteorites_cleaned %>% 
+  mutate(across(c(latitude, longitude), ~ coalesce(., 0))) 
+  
+
+## Selecting data of interest ----
+
+meteorites_cleaned <- meteorites_cleaned %>% 
+  # Removes meteorites less than 1000g in weight
+  filter(mass_g >= 1000) %>% 
+  #Arranges by year
+  arrange(year)
+
+# Check that data is valid ----
+
+meteorites_cleaned %>% 
+  verify(has_all_names("id", "name", "mass_g", "fall", "year", "latitude", "longitude")) %>% 
+  verify(latitude >= -90 & latitude <= 90) %>% 
+  verify(longitude >= -180 & longitude <= 180)
+
